b46d48da5cfb9dd65b081065058ecf07929dd61e
ashfinrahman <ashfinrahman05@gmail.com>
2025-10-02 15:46:10 -0400

    Added Login and Register page frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
 import Post from "./Post";
 import Header from "./Header";
-import {Routes} from "reactor-router-dom";
+import {Routes} from "react-router-dom";
 import {Route} from "react-router-dom";
-
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
-      <Route index element={
-        <main>
-          <Header />
-          <Post />
-          <Post />
-          <Post />
-        </main>
-      } />
-      <Route path={'/login'} element={
-        <div>login</div>
-      } />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />     
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
     </Routes>
 
   );
