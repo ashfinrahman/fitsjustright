aea1997326e4d6d2787b1f7634d8f346279e4643
ashfinrahman <ashfinrahman05@gmail.com>
2025-10-05 22:27:17 -0400

    a

--- client/src/Post.js
+++ client/src/Post.js
@@ -6,13 +6,13 @@ export default function Post() {
             <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTCTKSxSdy3LxTKHLDxuJYG9E7YIiZq-Z1WtEGDuMD38D_3HPd13FAaIw0IX0awCq0ZU1NZD9lpwEkFWGr9NzEYUEnNkbor6LHU3uSjBZVp_zbIo9D7XRQFVKA8" alt=""></img>
           </div>
           <div className="texts">
-            <h2>Full house battery backup coming later this year</h2>
+            <h2>Test Object</h2>
             <p className="info">
               <a className="author">Ashfin Rahman</a>
-              <time>2025-01-06 16:45</time>
+              <time>2025-10-01 16:45</time>
             </p>
             <p className="summary"></p>
-            <p>Today at its special launch event, home backup power giaasasass</p>
+            <p>Acne Studios Paint Stained Baggy Jeans</p>
           </div>
         </div>
     )
