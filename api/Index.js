024aa2ca6dff6a674dd1f2ca1f63d564082a7626
ashfinrahman <ashfinrahman05@gmail.com>
2026-03-23 18:37:05 -0400

    Set up database connection and user model for authentication

--- api/Index.js
+++ api/Index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
 
-app.get('/test', (req, res) => {
-    res.json('test ok3');
+const salt = bcrypt.genSaltSync(10);
+
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://ashfinrahman:MYEz8b7VhrgbfNZd@cluster0.dlxgojx.mongodb.net/?appName=Cluster0');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    try{
+        const userDoc = await User.create({username, password: bcrypt.hashSync(password, salt)});
+        res.json(userDoc);
+        console.log(userDoc);
+    } catch(e) {
+        res.status(400).json(e);
+        console.log(e);
+
+    }
+    
+    
+    
 });
 app.listen(4000);
-
+//mongodb+srv://ashfinrahman:MYEz8b7VhrgbfNZd@cluster0.dlxgojx.mongodb.net/?appName=Cluster0
