ae19cbbc3d7627a81687906eebf06c0ced175728
Ashfin Rahman <ashfinrahman05@gmail.com>
2026-03-24 00:43:33 -0400

    login and register finalized, frontend responsive to logins

--- api/Index.js
+++ api/Index.js
@@ -4,12 +4,15 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'asoidhfoa098sf09ausd9-8y';
 
-
-app.use(cors());
+app.use(cors({credentials:true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://ashfinrahman:MYEz8b7VhrgbfNZd@cluster0.dlxgojx.mongodb.net/?appName=Cluster0');
 
@@ -30,6 +33,32 @@ app.post('/login', async (req, res) => {
     const {username, password} = req.body;
     const userDoc = await User.findOne({username});
     const passOk = bcrypt.compareSync(password, userDoc.password);
+    //console.log(passOk);
+    if (passOk) {
+        //logged in
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+                id: userDoc._id,
+                username,
+            });
+        });
+    } else {
+        res.status(400).json('wrong credentials')
+    }
 });
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info)
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+})
+
 app.listen(4000);
 //mongodb+srv://ashfinrahman:MYEz8b7VhrgbfNZd@cluster0.dlxgojx.mongodb.net/?appName=Cluster0
